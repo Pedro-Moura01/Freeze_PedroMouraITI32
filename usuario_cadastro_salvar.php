33148f56317232a3016edf3cc95d7091b7ab805a
Pedro-Moura01 <pedromourapims9@gmail.com>
2024-10-15 11:47:40 -0300

    Codificado a criação de usuarios no BDA

--- usuario_cadastro_salvar.php
+++ usuario_cadastro_salvar.php
@@ -10,7 +10,28 @@
         <!-- Inclusão do topo da página -->
         <?php include "header_admin.php" ?>
 
-        <!-- Inclusão do rodapé -->
-        <?php include "footer_admin.php" ?>
-</body>
-</html>
+        <?php
+
+        //1° PASSO - INCLUSÃO AO BD
+        include "conexao_bd.php";
+
+        $login_usuario = $_POST["txtLoginUsuario"];
+        $senha_usuario = $_POST["txtSenhaUsuario"];
+        
+        //2° PASSO - CAPTURAR OS DADOS 
+        $hash = password_hash($senha_usuario, 1);
+        
+        //3° PASSO - COMANDO SQL
+        $sql = "INSERT INTO usuario(login_usuario,senha_usuario)";
+        $sql .= " VALUES('$login_usuario','$hash') ";
+
+        //4° PASSO - EXECUTAR NO BDA
+        if (executarComando ($sql))
+        {
+            echo "<h1>Usuário adicionado!</h1>";
+        }
+        else
+        {
+            echo "<h1>Usuárionão cadastrado!</h1>";
+        }
+?>
