5a8edd9084b61cbb5220bf2f88ad5d3251a3d738
Pedro-Moura01 <pedromourapims9@gmail.com>
2024-10-15 10:37:47 -0300

    Criando a pagina usuario salvar

---
+++ usuario_cadastro_salvar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <?php include "referencias.php" ?>
+</head>
+<body>
+        <!-- Inclusão do topo da página -->
+        <?php include "header_admin.php" ?>
+
+        <!-- Inclusão do rodapé -->
+        <?php include "footer_admin.php" ?>
+</body>
+</html>
